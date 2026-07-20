d26031c11ef9a449975b8f9fa343e5e6cc4a306c
DeVikingMark <DeVikingMark@users.noreply.github.com>
2026-07-20 02:27:27 +0000

    deploy: 984bcf51735fc4cfcf1cf520218ebd7186a757df

--- trait.impl/hotshot_task/dependency_task/trait.HandleDepOutput.js
+++ trait.impl/hotshot_task/dependency_task/trait.HandleDepOutput.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["hotshot_task_impls",[["impl&lt;TYPES: NodeType, I: NodeImplementation&lt;TYPES&gt; + 'static, V: Versions&gt; HandleDepOutput for <a class=\"struct\" href=\"hotshot_task_impls/quorum_vote/struct.VoteDependencyHandle.html\" title=\"struct hotshot_task_impls::quorum_vote::VoteDependencyHandle\">VoteDependencyHandle</a>&lt;TYPES, I, V&gt;",0],["impl&lt;TYPES: NodeType, V: Versions&gt; HandleDepOutput for <a class=\"struct\" href=\"hotshot_task_impls/quorum_proposal/handlers/struct.ProposalDependencyHandle.html\" title=\"struct hotshot_task_impls::quorum_proposal::handlers::ProposalDependencyHandle\">ProposalDependencyHandle</a>&lt;TYPES, V&gt;",0]]]]);
+    const implementors = Object.fromEntries([["hotshot_task_impls",[["impl&lt;TYPES: NodeType, I: NodeImplementation&lt;TYPES&gt; + 'static, V: Versions&gt; <a class=\"trait\" href=\"hotshot_task/dependency_task/trait.HandleDepOutput.html\" title=\"trait hotshot_task::dependency_task::HandleDepOutput\">HandleDepOutput</a> for <a class=\"struct\" href=\"hotshot_task_impls/quorum_vote/struct.VoteDependencyHandle.html\" title=\"struct hotshot_task_impls::quorum_vote::VoteDependencyHandle\">VoteDependencyHandle</a>&lt;TYPES, I, V&gt;",0],["impl&lt;TYPES: NodeType, V: Versions&gt; <a class=\"trait\" href=\"hotshot_task/dependency_task/trait.HandleDepOutput.html\" title=\"trait hotshot_task::dependency_task::HandleDepOutput\">HandleDepOutput</a> for <a class=\"struct\" href=\"hotshot_task_impls/quorum_proposal/handlers/struct.ProposalDependencyHandle.html\" title=\"struct hotshot_task_impls::quorum_proposal::handlers::ProposalDependencyHandle\">ProposalDependencyHandle</a>&lt;TYPES, V&gt;",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[656]}
+//{"start":59,"fragment_lengths":[958]}
